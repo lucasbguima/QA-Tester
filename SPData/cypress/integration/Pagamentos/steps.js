7dbb397633e516fd56b3276429ec3aba165d2cbf
criadordomkt <lucasbrandaoguimaraes@gmail.com>
2021-08-24 21:23:59 -0300

    Update steps.js

--- SPData/cypress/integration/Pagamentos/steps.js
+++ SPData/cypress/integration/Pagamentos/steps.js
@@ -1,5 +1,5 @@
 // Data Acess - Dados de Acesso Fictícios
-var url = "https://pagseguro.uol.com.br/checkout/nc/sender-identification.jhtml?t=6f96b76bec4816db6fe7aba1e713f9ec5fa0ad1e98c63846&e=true"
+var url = "https://pag.ae/7XsMFXAb1"
 var email = "teste@sandbox.pagseguro.com.br"
 var cartao = 4111111111111111
 var validade = 1230
@@ -12,12 +12,8 @@ var celular = 998000035
 //Scenario: Realizar pagamento com cartão de crédito
 
 Given(/^que o usuário acessou a página de pagamentos$/, () => {
-	cy.visit(url)
 });
 When(/^o usuário preencher os dados do formulário de pagamento$/, () => {
-	cy.get("#user").type(email)
-	cy.get("#hasNotAccount").click()
-	cy.get(".mainActionButton pagseguro-button largen").click()
 });
 When(/^selecionar pagamento com cartão de crédito$/, () => {
 });
@@ -25,7 +21,6 @@ When(/^confirmar o pagamento$/, () => {
 });
 Then(/^a página deve redirecionar para compra aprovada com sucesso$/, () => {
 });
-
 
 //Scenario: Realizar pagamento com cartão de débito
 
@@ -39,7 +34,6 @@ When(/^confirmar o pagamento$/, () => {
 });
 Then(/^a página deve redirecionar para compra aprovada com sucesso$/, () => {
 });
-
 
 //Scenario: Realizar pagamento com débito em conta
 
